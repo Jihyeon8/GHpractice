51ef0b7feeffc77c2c0bd71539dfda554314aef8
김지현 <p_blossom@naver.com>
2023-03-10 16:09:22 +0900

    This is Commit Message!

---
+++ src/main/java/kr/or/connect/ghproject/TestClass.java
@@ -0,0 +1,11 @@
+package kr.or.connect.ghproject;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! This is Github Practicing!");
+		// Github Practice!
+		
+	}
+
+}
